2ef8e91284798ad55829026b0e37c07883338ac5
Vladimir <shkodaonebob@gmail.com>
2024-01-04 15:34:10 +0600

    update currentTime component

--- src/features/CurrentTime/CurrentTime.tsx
+++ src/features/CurrentTime/CurrentTime.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const CurrentTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  setInterval(() => {
+    setCurrentTime(new Date());
+  }, 1000);
 
   return <span>{currentTime.toLocaleTimeString()}</span>;
 };
